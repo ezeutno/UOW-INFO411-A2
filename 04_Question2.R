a541ef3f0312d945955b7beeed277fd4a721d808
ezeutno <ivan.suratno@gmail.com>
2021-05-27 15:48:06 +0700

    Add predicted median hypotical customer

--- 04_Question2.R
+++ 04_Question2.R
@@ -5,7 +5,7 @@
 library(rpart)
 library(rpart.plot)
 
-tree <- rpart(credit.rating ~ ., data = dataset)
+tree <- rpart(credit.rating ~ ., data = training_set)
 summary(tree)
 
 # a Report the resulting tree
@@ -18,3 +18,16 @@ png("result/2_tree_rpart.png")
 rpart.plot(tree)
 dev.off()
 
+# b Based on this output, predict the credit rating of a hypothetical
+# “median” customer, i.e., one with the attributes listed in Table 1, showing the
+# steps involved.
+
+median_value <- apply(test_set,2,median)
+
+# Switch it into dataframe & transpose for prediction
+median_value <- as.data.frame(t(median_value))
+
+median_value$credit.rating
+
+# predict median value
+predict(tree, newdata=median_value)
